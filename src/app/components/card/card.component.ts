624c8567ad2a0fbc911fb5576298e18ad27a61c2
Your Name <vikasgc09@gmail.com>
2023-12-20 21:55:20 +0530

    Initial Commit

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
