a88cece7b3633f422d563767940c68b37e60cfe9
Vikas <Vikasgc09@gmail.com>
2023-12-22 16:36:25 +0530

    initial commit

--- src/app/routing-module/routing-module.module.ts
+++ src/app/routing-module/routing-module.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from '../components/footer/footer.component';
-import { HeaderComponent } from '../components/header/header.component';
+
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from '../app.component';
+
 import { CardComponent } from '../components/card/card.component';
 import { AboutComponent } from '../components/about/about.component';
 import { ContactsComponent } from '../components/contacts/contacts.component';
@@ -15,14 +14,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [
+  declarations: [ 
     
-
-
   ],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes),
+    RouterModule,
 
   ]
 })
