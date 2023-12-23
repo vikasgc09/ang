a4a99b97c9f4ff70f53a6b5926b51101727e0f54
Your Name <vikasgc09@gmail.com>
2023-12-23 11:37:20 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+import { CrudComponent } from './components/crud/crud.component';
 
 export const routes: Routes = [
     { path: 'card', component: CardComponent },
     { path: 'about', component: AboutComponent },
     { path: 'contacts', component: ContactsComponent },
+    { path: 'crud', component: CrudComponent },
   ];
